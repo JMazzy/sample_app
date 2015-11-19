88e6d80cda634e695948617c8d1836bdb602deb7
Joshua Masland-Sarani <j.masland@gmail.com>
2015-11-19 09:07:19 -0500

    Add account activations

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,6 +55,17 @@ class User < ActiveRecord::Base
     admin
   end
 
+  #Activates an account
+  def activate
+  	update_attribute(:activated, true)
+  	update_attribute(:activated_at, Time.zone.now)
+  end
+
+  #Sends activation email
+  def send_activation_email
+  	UserMailer.account_activation(self).deliver_now
+  end
+
   private
 
   def downcase_email
